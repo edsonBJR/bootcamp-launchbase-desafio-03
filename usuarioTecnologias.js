294fdb70ed8a816076148f5d9a56cecda2b06b8c
edsonBJR <ebjr.tux@gmail.com>
2020-06-16 14:24:09 -0300

    Ajustado arquivo despesasReceitas.js

--- usuarioTecnologias.js
+++ usuarioTecnologias.js
@@ -26,6 +26,24 @@ const usuarios = [{
         ]
     }
 ]
+
+// Exibindo em tela cada usuarios da coleção de usuarios, 
+// o método join permite colocar um separador dentro dos itens do objeto no caso a virgula
 for (let usuario of usuarios) {
-    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias}`)
+    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`)
+}
+
+function checaSeUsuarioUsaCss(usuario) {
+    for (let tecnologia of usuario.tecnologias) {
+        if (tecnologia == 'CSS') return true
+    }
+    return false
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+    const usuarioTrabalhaComCss = checaSeUsuarioUsaCss(usuarios[i])
+
+    if (usuarioTrabalhaComCss) {
+        console.log(`O usuario ${usuarios[i].nome} trabalha com CSS`)
+    }
 }
